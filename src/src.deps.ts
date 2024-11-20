3c87337ab2010e51623f063859e0113751fa59ee
mcgear <michael.gearhardt@gmail.com>
2024-11-20 12:15:37 -0700

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,10 +1,10 @@
-export * from 'jsr:@fathym/common@0.2.50';
+export * from 'jsr:@fathym/common@0.2.168';
 
-export * from 'jsr:@fathym/eac@0.2.13';
-export * from 'jsr:@fathym/eac@0.2.13/steward';
-export * from 'jsr:@fathym/eac@0.2.13/steward/clients';
-export * from 'jsr:@fathym/eac@0.2.13/steward/status';
+export * from 'jsr:@fathym/eac@0.2.27';
+export * from 'jsr:@fathym/eac@0.2.27/steward';
+export * from 'jsr:@fathym/eac@0.2.27/steward/clients';
+export * from 'jsr:@fathym/eac@0.2.27/steward/status';
 
 export * from 'jsr:@fathym/eac-deno-kv@0.0.4';
 
-export * from 'jsr:@fathym/eac-dfs@0.0.15';
+export * from 'jsr:@fathym/eac-dfs@0.0.26';
